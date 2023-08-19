f40385476f42a0357b32a23df58dd402b9de1660
alikiminza <ali.kiminza@student.moringaschool.com>
2023-08-19 13:15:52 +0300

    implemented required changes

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if username == "admin":
+       password == 12345 
+       print("access granted")
+    elif username == "ADMIN":
+         password == 12345
+         print("access granted")   
+    else :
+        print("access denied")   
+    
+#admin_login("ADMIN", 12345)    
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40 :
+        print("It's brisk out there!")
+    elif temperature <= 65 :
+        print("It's a little chilly out there!")
+    elif temperature > 85 :
+        print("It's too dang out there! ")    
+    else:
+        print("It's perfect out there!")
+
+#hows_the_weather(90)        
 
 def fizzbuzz(num):
     # your code here
